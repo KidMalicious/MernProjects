dfcc66760df97fa3b98c2b5246dc301647586880
KidMaliciouz <akf.behopi@gmail.com>
2021-05-24 02:12:40 -0700

    add userItem and userList

--- mern-s5/src/user/components/UsersList.js
+++ mern-s5/src/user/components/UsersList.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
 import UserItem from './UserItem'
+import Card from '../../shared/components/UIElements/Card'
 import './UsersList.css'
 
 const UsersList = (props) => {
-    if (props.item.length === 0) {
+    if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>
-                    No users found.
-                </h2>
+                <Card>
+                    <h2>
+                        No users found.
+                    </h2>
+                </Card>
             </div>
         )
     }
     return (
-        <ul>
+        <ul className="users-list">
             {props.items.map(user => <UserItem
                 key={user.id}
                 id={user.id}
