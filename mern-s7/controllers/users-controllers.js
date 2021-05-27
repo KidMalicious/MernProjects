96367b96fe38688e752e2dc96e0ad42513f3a481
KidMaliciouz <akf.behopi@gmail.com>
2021-05-27 14:56:04 -0700

    express validation for POST and PATCH requests

--- mern-s7/controllers/users-controllers.js
+++ mern-s7/controllers/users-controllers.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid')
 
+const { validationResult } = require('express-validator')
+
 const HttpError = require('../models/http-error')
 
 const DUMMY_USERS = [
@@ -18,6 +20,12 @@ const getUsers = (req, res, next) => {
 }
 
 const signup = (req, res, next) => {
+    const error = validationResult(req)
+
+    if (!error.isEmpty()) {
+        throw new HttpError('Invalid inputs passed, please check your data.', 422)
+    }
+    
     const { name, email, password } = req.body
 
     const hasUser = DUMMY_USERS.find(u => u.email === email)
