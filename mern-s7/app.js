42d832dac27e0579b072565458ad24ab42bcc4a2
KidMaliciouz <akf.behopi@gmail.com>
2021-05-27 02:17:19 -0700

    404 if there is no route found

--- mern-s7/app.js
+++ mern-s7/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const HttpError = require('./models/http-error')
 const placesRoutes = require('./routes/places-route')
 const usersRoutes = require('./routes/users-route')
 
 const app = express();
 
+app.use(bodyParser.json())
+
 app.use("/api/places",placesRoutes)
 app.use("/api/users", usersRoutes)
+
+app.use((req, res, next) => {
+    const error = new HttpError('Could not find this route.', 404)
+    throw error
+})
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
