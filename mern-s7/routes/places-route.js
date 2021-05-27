6ca8683db1b656b5c9c0c461065fb2ef8cca2023
KidMaliciouz <akf.behopi@gmail.com>
2021-05-26 18:53:29 -0700

    add error model

--- mern-s7/routes/places-route.js
+++ mern-s7/routes/places-route.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const HttpError = require('../models/http-error')
+
 const router = express.Router();
 
 const DUMMY_PLACES = [
@@ -19,9 +22,7 @@ router.get('/:pid', (req, res, next) => {
     const placeId = req.params.pid
     const place = DUMMY_PLACES.find(p => p.id === placeId)
     if (!place){
-        const error = new Error('Could not find a place for the provided id')
-        error.code = 404
-        throw error
+        throw new HttpError('Could not find a place for the provided id', 404)
     }
     res.json({
         place
@@ -32,9 +33,7 @@ router.get('/user/:uid', (req, res, next) => {
     const userId = req.params.uid
     const place = DUMMY_PLACES.find(p => p.creator === userId)
     if (!place){
-        const error = new Error('Could not find a place for the provided id')
-        error.code = 404
-        return next(error)
+        return next(new HttpError('Could not find a place for the provided id', 404))
     }
     res.json({
         place
